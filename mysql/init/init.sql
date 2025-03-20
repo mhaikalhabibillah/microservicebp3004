de4a07969fc704c3605280e0a25a94c139b0380e
M.Haikal Habibillah <168246758+mhaikalhabibillah@users.noreply.github.com>
2025-03-20 21:03:41 +0700

    Add files via upload

---
+++ mysql/init/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS dbecommerce_produk;
+CREATE DATABASE IF NOT EXISTS dbecommerce_orders;
+CREATE DATABASE IF NOT EXISTS dbecommerce_payment;
+CREATE DATABASE IF NOT EXISTS dbecommerce_customer;
