0ab553bbb9edb00de728abc5f2d41634cd66da8a
M.Haikal Habibillah <168246758+mhaikalhabibillah@users.noreply.github.com>
2025-03-20 21:02:28 +0700

    Add files via upload

---
+++ order-service/src/main/java/com/teknologiinformasi/restapi/order/event/OrderCreatedEvent.java
@@ -0,0 +1,60 @@
+package com.teknologiinformasi.restapi.order.event;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+public class OrderCreatedEvent implements Serializable {
+   private String orderId;
+   private Long productId;
+   private int quantity;
+   private LocalDateTime orderDate;
+   private String orderStatus; // misal: CREATED
+   public OrderCreatedEvent() {}
+   public OrderCreatedEvent(String orderId, Long productId, int quantity, LocalDateTime orderDate, String orderStatus) {
+       this.orderId = orderId;
+       this.productId = productId;
+       this.quantity = quantity;
+       this.orderDate = orderDate;
+       this.orderStatus = orderStatus;
+   }
+   // Getters & Setters
+   public String getOrderId() {
+       return orderId;
+   }
+   public void setOrderId(String orderId) {
+       this.orderId = orderId;
+   }
+   public Long getProductId() {
+       return productId;
+   }
+   public void setProductId(Long productId) {
+       this.productId = productId;
+   }
+   public int getQuantity() {
+       return quantity;
+   }
+   public void setQuantity(int quantity) {
+       this.quantity = quantity;
+   }
+   public LocalDateTime getOrderDate() {
+       return orderDate;
+   }
+   public void setOrderDate(LocalDateTime orderDate) {
+       this.orderDate = orderDate;
+   }
+   public String getOrderStatus() {
+       return orderStatus;
+   }
+   public void setOrderStatus(String orderStatus) {
+       this.orderStatus = orderStatus;
+   }
+   @Override
+   public String toString() {
+       return "OrderCreatedEvent{" +
+               "orderId=" + orderId +
+               ", productId=" + productId +
+               ", quantity=" + quantity +
+               ", orderDate=" + orderDate +
+               ", orderStatus='" + orderStatus + '\'' +
+               '}';
+   }
+}
