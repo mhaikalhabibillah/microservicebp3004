09a1b9e5a79cf36e6ab2dcd6606b790d3aae5089
M.Haikal Habibillah <168246758+mhaikalhabibillah@users.noreply.github.com>
2025-04-16 18:32:50 +0700

    Add files via upload

---
+++ restapi/src/main/java/com/teknologiinformasi/restapi/repository/ProdukRepository.java
@@ -0,0 +1,11 @@
+package com.teknologiinformasi.restapi.repository;
+import com.teknologiinformasi.restapi.model.Produk;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProdukRepository extends JpaRepository<Produk, Long> {
+}
+
+
